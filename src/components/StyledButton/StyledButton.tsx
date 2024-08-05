612973e4ab1c234e15aaf059346504b0cb8961e5
Valeria <valleriademelo@gmail.com>
2024-08-05 19:05:06 -0300

    feat: add click handlers to StyledButton for download and email redirection

--- src/components/StyledButton/StyledButton.tsx
+++ src/components/StyledButton/StyledButton.tsx
@@ -3,35 +3,34 @@ import { ReactNode } from "react"
 
 interface StyledButtonProps {
   children: ReactNode,
-  onClick:() => void
+  onClick?: () => void
 }
 
 const StyledButton: React.FC<StyledButtonProps> = ({ children, onClick }) => {
-  
-  const StyledButton = styled("button")(({theme}) => ({
-    backgroundColor: "transparent",
-    border: `1px solid ${theme.palette.primary.contrastText}`,
-    borderRadius: "3px",
-    padding: "5px 15px",
-    width: "100%",
-    color: theme.palette.primary.contrastText,
-    display: "inline-flex",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: "10px",
-    '&:hover': {
-      backgroundColor: theme.palette.secondary.light
-    }
 
-  }))
+    const StyledButton = styled("button")(({ theme }) => ({
+        backgroundColor: "transparent",
+        border: `1px solid ${theme.palette.primary.contrastText}`,
+        borderRadius: "3px",
+        padding: "5px 15px",
+        width: "100%",
+        color: theme.palette.primary.contrastText,
+        display: "inline-flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "10px",
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.light
+        }
+    }))
 
-  return (
-    <>
-      <StyledButton onClick={onClick}>
-        {children}
-      </StyledButton>
-    </>
-  )
+    return (
+        <>
+            <StyledButton onClick={onClick}>
+                {children}
+            </StyledButton>
+        </>
+    )
 }
 
 export default StyledButton
--- src/pages/Home/sections/Hero/hero.tsx
+++ src/pages/Home/sections/Hero/hero.tsx
@@ -1,18 +1,13 @@
-import styled from "@emotion/styled"
-import Avatar from "../../../../assets/images/avatar.png"
-import Grid from "@mui/material/Grid"
-import { Box, Container, Typography } from "@mui/material"
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import { Box, Container, Grid, Typography, styled } from "@mui/material";
+import Avatar from "../../../../assets/images/avatar.png";
 import DownloadIcon from '@mui/icons-material/Download';
-import theme from '../../../../theme';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import StyledButton from "../../../../components/StyledButton/StyledButton";
 import { AnimatedBackground } from "../../../../components/StyledButton/AnimatedBackground/AnimatedBackground";
-import { Redo } from "@mui/icons-material";
-
-
+import theme from "../../../../theme";
 
 const Hero = () => {
-  const StyleHero = styled('div')(() => ({
+  const StyledHero = styled('div')(() => ({
     backgroundColor: theme.palette.primary.main,
     height: '100vh',
     display: 'flex',
@@ -25,55 +20,69 @@ const Hero = () => {
     }
   }));
 
-  const StyledImg = styled("img")(() => ({
-    width: "75%",
+   const handleContactClick = () => {
+    setTimeout(() => {
+      window.location.href = "mailto:valeriamelo.dev@gmail.com";
+    }, 0);
+  };
+
+  const handleDownloadClick = () => {
+    const link = document.createElement('a');
+    link.href = '../../../../../public/curriculo.pdf'; 
+    link.download = 'curriculo-valeria-melo.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+const StyledImg = styled("img")(() => ({
+    width: "80%",
     borderRadius: "50%",
     border: `1px solid ${theme.palette.primary.contrastText}`,
-
-  }))
-
+}))
+ 
   return (
     <>
-      <StyleHero>
-        <Container maxWidth="lg">
-       <Grid container spacing={2}>
-            <Grid item xs={12} md={5}>
-              <Box position="relative">
-                <Box position="absolute" width={"150%"} top={-100} right={0}>
-                 <AnimatedBackground />
-                </Box>  
-                <Box position="relative" textAlign="center">
-                 <StyledImg src={Avatar} />
-                </Box>
+    <StyledHero>
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={5}>
+            <Box position="relative">
+              <Box position="absolute" width={"150%"} top={-100} right={0}>
+                <AnimatedBackground />
               </Box>
+              <Box position="relative" textAlign="center">
+                <StyledImg src={Avatar} />
+              </Box>
+            </Box>
           </Grid>
           <Grid item xs={12} md={7}>
               <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Valeria Melo</Typography>
               <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
-              <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
-                <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                  <StyledButton>
-                    <DownloadIcon />
-                    <Typography>
-                       Dowload CV 
-                    </Typography>
-                    </StyledButton>
-                </Grid>
-                <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                  <StyledButton>
-                    <MailOutlineIcon />
-                    <Typography>
-                       Contact me
-                    </Typography>
-                </StyledButton>
-                </Grid>
+            <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
+              <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                <StyledButton onClick={handleDownloadClick}>
+                  <DownloadIcon />
+                  <Typography>
+                    Download CV
+                  </Typography>
+                  </StyledButton>
               </Grid>
+              <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                <StyledButton onClick={handleContactClick}>
+                  <MailOutlineIcon />
+                  <Typography>
+                    Contact me
+                  </Typography>
+                </StyledButton>
+              </Grid>
+            </Grid>
           </Grid>
-          </Grid>
-          </Container>
-      </StyleHero>
+        </Grid>
+      </Container>
+    </StyledHero>
     </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
