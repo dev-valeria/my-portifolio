671512355ef992897ea0f6b5ebbfb6a57a5f4288
Valeria <valleriademelo@gmail.com>
2024-08-06 16:54:13 -0300

    Refactored StyledButton to create two new buttons for About Me section

--- src/components/StyledButton/StyledButton.tsx
+++ src/components/StyledButton/StyledButton.tsx
@@ -1,36 +1,77 @@
-import { styled } from "@mui/material"
-import { ReactNode } from "react"
+import React from 'react';
+import { styled } from '@mui/material/styles';
 
 interface StyledButtonProps {
-  children: ReactNode,
-  onClick?: () => void
+  children: React.ReactNode;
+  onClick?: () => void;
 }
 
-const StyledButton: React.FC<StyledButtonProps> = ({ children, onClick }) => {
+const StyledButtonExperience = styled('button')(({ theme }) => ({
+  backgroundColor: "transparent",
+  border: `1px solid ${theme.palette.primary.main}`,
+  width: "177px",
+  height: "115px",
+  color: theme.palette.primary.main,
+  '&:hover': {
+    backgroundColor: theme.palette.secondary.light,
+  },
+}));
 
-    const StyledButton = styled("button")(({ theme }) => ({
-        backgroundColor: "transparent",
-        border: `1px solid ${theme.palette.primary.contrastText}`,
-        borderRadius: "3px",
-        padding: "5px 15px",
-        width: "100%",
-        color: theme.palette.primary.contrastText,
-        display: "inline-flex",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: "10px",
-        '&:hover': {
-            backgroundColor: theme.palette.secondary.light
-        }
-    }))
+const StyledButtonEducation = styled('button')(({ theme }) => ({
+  backgroundColor: "transparent",
+  border: `1px solid ${theme.palette.primary.main}`, 
+  width: "177px",
+  height: "115px",
+  color: theme.palette.primary.main,
+  alignItems: "center",
+  justifyContent: "center",
+  
+  '&:hover': {
+    backgroundColor: theme.palette.secondary.light,
+  },
+}));
 
-    return (
-        <>
-            <StyledButton onClick={onClick}>
-                {children}
-            </StyledButton>
-        </>
-    )
-}
 
-export default StyledButton
+const StyledButtonDefault = styled('button')(({ theme }) => ({
+  backgroundColor: "transparent",
+  border: `1px solid ${theme.palette.primary.contrastText}`,
+  borderRadius: "3px",
+  padding: "5px 15px",
+  width: "100%",
+  color: theme.palette.primary.contrastText,
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "10px",
+  '&:hover': {
+    backgroundColor: theme.palette.secondary.light,
+  },
+}));
+
+
+const StyledButtonDefaultComponent: React.FC<StyledButtonProps> = ({ children, onClick }) => {
+  return (
+    <StyledButtonDefault onClick={onClick}>
+      {children}
+    </StyledButtonDefault>
+  );
+};
+
+const StyledButtonEducationComponent: React.FC<StyledButtonProps> = ({ children, onClick }) => {
+  return (
+    <StyledButtonEducation onClick={onClick}>
+      {children}
+    </StyledButtonEducation>
+  );
+};
+
+const StyledButtonExperienceComponent: React.FC<StyledButtonProps> = ({ children, onClick }) => {
+  return (
+    <StyledButtonExperience onClick={onClick}>
+      {children}
+    </StyledButtonExperience>
+  );
+};
+
+export { StyledButtonDefaultComponent as StyledButtonDefault, StyledButtonExperienceComponent as StyledButtonExperience, StyledButtonEducationComponent as StyledButtonEducation };
+
--- src/pages/Home/sections/Hero/hero.tsx
+++ src/pages/Home/sections/Hero/hero.tsx
@@ -1,26 +1,22 @@
-import { Box, Container, Grid, Typography, styled } from "@mui/material";
+import { Box, Container, Grid, Typography, styled, useMediaQuery } from "@mui/material";
 import Avatar from "../../../../assets/images/avatar.png";
 import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import StyledButton from "../../../../components/StyledButton/StyledButton";
 import { AnimatedBackground } from "../../../../components/StyledButton/AnimatedBackground/AnimatedBackground";
 import theme from "../../../../theme";
+import { StyledButtonDefault } from "../../../../components/StyledButton/StyledButton";
 
 const Hero = () => {
-  const StyledHero = styled('div')(() => ({
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+  const StyledHero = styled('div')(({ theme }) => ({
     backgroundColor: theme.palette.primary.main,
     height: '100vh',
-    display: 'flex',
-    alignItems: 'center',
-    [theme.breakpoints.up('xs')]: {
-      paddingTop: '100%'
-    },
-    [theme.breakpoints.up('md')]: {
-      paddingTop: '0'
-    }
+    display: "flex",
+    alignItems: "center",
   }));
 
-   const handleContactClick = () => {
+  const handleContactClick = () => {
     setTimeout(() => {
       window.location.href = "mailto:valeriamelo.dev@gmail.com";
     }, 0);
@@ -28,23 +24,24 @@ const Hero = () => {
 
   const handleDownloadClick = () => {
     const link = document.createElement('a');
-    link.href = '../../../../../public/curriculo.pdf'; 
+    link.href = '/path/to/curriculo.pdf'; 
     link.download = 'curriculo-valeria-melo.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
 
-const StyledImg = styled("img")(() => ({
+  const StyledImg = styled("img")(({ theme }) => ({
     width: "80%",
     borderRadius: "50%",
     border: `1px solid ${theme.palette.primary.contrastText}`,
-}))
- 
+  }));
+
   return (
     <>
-    <StyledHero>
-      <Container maxWidth="lg">
+         <StyledHero>
+    <Container maxWidth="lg">
+    
         <Grid container spacing={2}>
           <Grid item xs={12} md={5}>
             <Box position="relative">
@@ -56,32 +53,43 @@ const StyledImg = styled("img")(() => ({
               </Box>
             </Box>
           </Grid>
-          <Grid item xs={12} md={7}>
-              <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Valeria Melo</Typography>
-              <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
+          <Grid item xs={12} md={7} 
+            sx={{
+              pt: { xs: '13px !important', md: '80px !important'  }, 
+              pr: { xs: '46px !important', md: '84px !important'  }, 
+              pl: { xs: '63px !important', md: '63px !important' }
+            }}
+          >
+            <Typography color="primary.contrastText" variant="h2" textAlign="center" pb={2}>
+              Valeria Melo
+            </Typography>
+            <Typography color="primary.contrastText" variant={isMobile ? 'h5' : 'h3'}  textAlign="center">
+              I'm a Software Engineer
+            </Typography>
             <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
               <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                <StyledButton onClick={handleDownloadClick}>
+                <StyledButtonDefault onClick={handleDownloadClick}>
                   <DownloadIcon />
-                  <Typography>
+                  <Typography fontSize={14}>
                     Download CV
                   </Typography>
-                  </StyledButton>
+                </StyledButtonDefault>
               </Grid>
               <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                <StyledButton onClick={handleContactClick}>
+                <StyledButtonDefault onClick={handleContactClick}>
                   <MailOutlineIcon />
-                  <Typography>
+                  <Typography fontSize={14}>
                     Contact me
                   </Typography>
-                </StyledButton>
+                </StyledButtonDefault>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Container>
-    </StyledHero>
+    </Container>
+      </StyledHero >
     </>
+
   );
 }
 
