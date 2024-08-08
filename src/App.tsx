579c8227da87fa7da6724a5e1581f32c2e4cffd0
Valeria <valleriademelo@gmail.com>
2024-08-07 22:02:57 -0300

    feat: implement navigation route for About Me page in the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import Home from "./pages/Home/Home"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from "./components/NavBar/NavBar"
+import AboutMe from './pages/Home/sections/About/AboutMe';
+import Hero from './pages/Home/sections/Hero/hero';
 
 
 const App = () => {
 
 
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      
+      <NavBar />
+      <Hero />
+      <Routes>
+        <Route path="/about" element={<AboutMe />} />
+      </Routes>
+    </Router>
+    
   )
 }
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,23 +1,28 @@
-import { AppBar, MenuItem, Toolbar, styled } from "@mui/material"
+// NavBar.js ou NavBar.tsx
+import { AppBar, MenuItem, Toolbar, styled } from '@mui/material';
+import { Link as ScrollLink } from 'react-scroll';
 
+const StyledToolbar = styled(Toolbar)(() => ({
+  display: 'flex',
+  justifyContent: 'space-evenly',
+}));
 
 const NavBar = () => {
-    const StyledToobar = styled(Toolbar)(() => ({
-        display: "flex",
-        justifyContent: "space-evenly"
-    }))
+  return (
+    <AppBar position="absolute">
+      <StyledToolbar>
+        <ScrollLink to="about" smooth={true} duration={500} style={{ cursor: 'pointer' }}>
+          <MenuItem>About</MenuItem>
+        </ScrollLink>
+        <ScrollLink to="skills" smooth={true} duration={500} style={{ cursor: 'pointer' }}>
+          <MenuItem>Skills</MenuItem>
+        </ScrollLink>
+        <ScrollLink to="projects" smooth={true} duration={500} style={{ cursor: 'pointer' }}>
+          <MenuItem>Projects</MenuItem>
+        </ScrollLink>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
 
-    return (
-        <>
-            <AppBar position="absolute">
-                <StyledToobar>
-                    <MenuItem>About</MenuItem>
-                    <MenuItem>Skills</MenuItem>
-                    <MenuItem>Projects</MenuItem>
-                </StyledToobar>
-            </AppBar>
-        </>
-    )
-}
-
-export default NavBar
+export default NavBar;
