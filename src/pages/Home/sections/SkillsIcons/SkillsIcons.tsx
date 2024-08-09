24b6c518736c8f1957436da8106c8e77d29056bf
Valeria <valleriademelo@gmail.com>
2024-08-08 21:57:22 -0300

    feat: improve design with smaller buttons, larger title, and updated background

--- src/pages/Home/sections/SkillsIcons/SkillsIcons.tsx
+++ src/pages/Home/sections/SkillsIcons/SkillsIcons.tsx
@@ -1,6 +1,6 @@
-import { Container, Grid, Typography, Box } from '@mui/material';
+import { Container, Grid, Typography, Box, styled,  keyframes } from '@mui/material';
 
-
+// Dados das habilidades
 const skillsData = [
   { icon: <img src="https://img.icons8.com/?size=100&id=71257&format=png&color=000000" alt="Angular" />  },
   { icon: <img src="https://img.icons8.com/?size=100&id=20909&format=png&color=000000" alt="Html"/> },
@@ -14,21 +14,55 @@ const skillsData = [
   { icon: <img src="https://img.icons8.com/?size=100&id=33039&format=png&color=000000" alt="AWS"/> },
   { icon: <img src="https://img.icons8.com/?size=100&id=gFw7X5Tbl3ss&format=png&color=000000" alt="Material UI"/> },
   { icon: <img src="https://img.icons8.com/?size=100&id=zfHRZ6i1Wg0U&format=png&color=000000" alt="Figma"/> },
-
 ];
+
+// Defina a animação
+const fadeInAndScaleUp = keyframes`
+  0% {
+    opacity: 0;
+    transform: scale(0.9);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
+
+// Estilos para a imagem do ícone
+const IconImage = styled('img')(({ theme }) => ({
+  width: '60px',
+  height: 'auto',
+  marginBottom: theme.spacing(2),
+}));
+
+// Estilos para o contêiner de habilidades
+const SkillBox = styled(Box)(({ theme }) => ({
+  textAlign: 'center',
+  py: 2,
+  backgroundColor: '#fff',
+  borderRadius: '8px',
+  boxShadow: theme.shadows[1],
+  animation: `${fadeInAndScaleUp} 1s ease-out`,
+}));
 
 const SkillsIcons = () => {
   return (
-    <Container maxWidth="lg" id="skills" sx={{ pt: 2, pb: 5 }}>
-      <Typography variant="h2" textAlign="center" paddingBottom={5} gutterBottom>
-        Skills
-      </Typography>
-      <Grid container spacing={3}>
+    <Container maxWidth="lg" id="skills" sx={{ pt: 4, pb: 6, backgroundColor: '#f5f5f5', mb: 8 }}>
+      <Typography variant="h1" align="center" gutterBottom>
+          Skills
+        </Typography>
+      <Grid container spacing={3} justifyContent="center">
         {skillsData.map((skill, index) => (
-          <Grid item xs={2}  key={index} sx={{ pb: 4, mw:8}}>
-            <Box display="flex" flexDirection="column-reverse" alignItems="center">
-              {skill.icon}
-            </Box>
+          <Grid item xs={2} sm={2} md={2} lg={2} xl={2}  key={index}>
+            <SkillBox
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <IconImage src={skill.icon.props.src} alt={skill.icon.props.alt} />
+              <Typography variant="h6" sx={{ mb: 2 }}>{skill.icon.props.alt}</Typography>
+            </SkillBox>
           </Grid>
         ))}
       </Grid>
@@ -37,8 +71,3 @@ const SkillsIcons = () => {
 };
 
 export default SkillsIcons;
-
-
-
-
-
