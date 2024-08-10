3abb3f6d36f5a1623d5ab9feb59067c5d679acf9
Valeria <valleriademelo@gmail.com>
2024-08-09 22:57:34 -0300

    feat: Add translation files for About Me, Skills, and Projects sections

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,25 +1,72 @@
-// NavBar.js ou NavBar.tsx
-import { AppBar, MenuItem, Toolbar, styled } from '@mui/material';
+import { AppBar, MenuItem, Select, SelectChangeEvent, Toolbar, styled } from '@mui/material';
 import { Link as ScrollLink } from 'react-scroll';
+import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
+import brasil from '../../assets/images/brasil.png';
+import estadosUnidos from '../../assets/images/estadosUnidos.png';
+import espanha from '../../assets/images/espanha.png';
 
 const StyledToolbar = styled(Toolbar)(() => ({
   display: 'flex',
   justifyContent: 'space-evenly',
 }));
 
+const StyledImg = styled("img")(({ }) => ({
+  width: '24px',
+  height: 'auto',
+  marginRight: '8px',
+}));
+
 const NavBar = () => {
+  const { t, i18n } = useTranslation();
+  const [language, setLanguage] = useState(i18n.language);
+
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    const selectedLanguage = event.target.value;
+    setLanguage(selectedLanguage);
+    i18n.changeLanguage(selectedLanguage);
+  };
+
   return (
     <AppBar position="absolute">
       <StyledToolbar>
         <ScrollLink to="about" smooth={true} duration={500} style={{ cursor: 'pointer' }}>
-          <MenuItem>About</MenuItem>
+          <MenuItem>{t('about')}</MenuItem>
         </ScrollLink>
         <ScrollLink to="skills" smooth={true} duration={500} style={{ cursor: 'pointer' }}>
-          <MenuItem>Skills</MenuItem>
+          <MenuItem>{t('skills')}</MenuItem>
         </ScrollLink>
-        <ScrollLink to="projects" smooth={true} duration={500} style={{ cursor: 'pointer' }}>
-          <MenuItem>Projects</MenuItem>
+        <ScrollLink to="projectsSection" smooth={true} duration={500} style={{ cursor: 'pointer' }}>
+          <MenuItem>{t('projectsSection')}</MenuItem>
         </ScrollLink>
+        <Select
+          value={language}
+          onChange={handleChange}
+          variant="outlined"
+          size="small"
+          color="secondary"
+          sx={{
+            width: '67px',
+            minWidth: '40px',
+            height: '32px',
+            '& .MuiSelect-select': {
+              display: 'flex',
+              alignItems: 'center',
+              padding: '0 8px',
+            },
+          }}
+          renderValue={() => (
+            <StyledImg src={
+              language === 'en' ? estadosUnidos
+              : language === 'es' ? espanha
+              : brasil
+            } alt="Language Flag" />
+          )}
+        >
+          <MenuItem value="pt"><StyledImg src={brasil} alt="Brazilian Flag" /></MenuItem>
+          <MenuItem value="en"><StyledImg src={estadosUnidos} alt="US Flag" /></MenuItem>
+          <MenuItem value="es"><StyledImg src={espanha} alt="Spanish Flag" /></MenuItem>
+        </Select>
       </StyledToolbar>
     </AppBar>
   );
