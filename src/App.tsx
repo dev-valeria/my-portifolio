22fcafd52639eaa021afedf262a189d0bb833a9c
Valeria <valleriademelo@gmail.com>
2024-08-09 22:54:16 -0300

    feat:Adicionado suporte para múltiplos idiomas (inglês, português e espanhol) no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,23 @@ import AboutMe from './pages/Home/sections/About/AboutMe';
 import Hero from './pages/Home/sections/Hero/hero';
 import SkillsIcons from "./pages/Home/sections/SkillsIcons/SkillsIcons";
 import Projects from './pages/Home/sections/Projects/projects';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n/i18n';
 
 const App = () => {
 
 
   return (
     <Router>
+    <I18nextProvider i18n={i18n}>
       <NavBar />
       <Hero />
       <Routes>
         <Route path="/about" element={<AboutMe />} />
         <Route path="/skills" element={<SkillsIcons />} />
         <Route path="/projects" element={<Projects />} />
-      </Routes>
+        </Routes>
+            </I18nextProvider>
     </Router>
     
   )
